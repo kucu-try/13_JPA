3745126809cb5d5bdf8137a950d531f79d60e30b
kucu-try <baekfive@gmail.com>
2024-02-08 17:31:23 +0900

    react fetct

--- ReactSpring/src/main/frontend/src/App.js
+++ ReactSpring/src/main/frontend/src/App.js
@@ -2,25 +2,32 @@ import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 
 function App() {
-  const [hello, setHello] = useState('')
+  const [hello, setHello] = useState("")
 
   useEffect(() => {
     // axios.get('/api/hello')
     //     .then(response => setHello(response.data))
     //     .catch(error => console.log(error))
-     fetchData();
-
+    const fetchData = async () => {
+        try {
+            const response = await fetch('/api/hello');
+            if (!response.ok){
+                throw new Error("에러다 뭉청한 눔");
+            }
+            const data =  await response.text();
+            setHello(data);
+        }catch (error){
+            console.log('에러다 이 놈아');
+        }
+    };
+    fetchData();
   }, []);
 
-  const fetchData = async () => {
-          const response = await fetch('/hello');
-          const jsonData = await response.json();
-          setHello(jsonData);
-      };
+
 
   return (
       <div>
-        백엔드 데이터임 : {hello}
+        백엔드 데이터 : {hello}
       </div>
   );
 }
